8912aae89518f6b390b102bfb855db644e56a84e
david <david.hasiholan@gmail.com>
2019-03-08 13:26:08 +0700

    adding delete user

--- src/components/UserLink.js
+++ src/components/UserLink.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 import '../styles/App.css';
 
@@ -18,10 +19,22 @@ const UserLink = React.createClass({
 			  }}>
 				  {this.props.nama}
 			</Link>
+			<a href="#" onClick={() => this.deleteProfile(this.props.id)}> [x] </a>
 		  </li>
 		)
-	}
+	},
 
+	deleteProfile (profile_id) {
+		let url = "http://localhost:3001/user/" + profile_id;
+
+		// Post request ..
+		return axios.delete(url)
+				.then(response => {
+					console.log(response)
+		}).catch(failure => {
+			console.log (failure)
+		});
+	}
 })
 
 export default UserLink;
